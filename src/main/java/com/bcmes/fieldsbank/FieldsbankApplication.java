7620ea974834436ffab1e90bb8d3e7f131535940
brunosilvapicpay <bruno.csilva@picpay.com>
2025-08-03 15:20:32 -0300

    study: initial commit

---
+++ src/main/java/com/bcmes/fieldsbank/FieldsbankApplication.java
@@ -0,0 +1,13 @@
+package com.bcmes.fieldsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldsbankApplication.class, args);
+	}
+
+}
